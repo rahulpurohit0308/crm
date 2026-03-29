836e6bc4b70eccafc54edd04c268cc45df28b7a4
Rahul Purohit <rahulpurohit0308@gmail.com>
2026-03-30 02:17:34 +0530

    start

---
+++ crm-backend/src/test/java/com/crm/crm_backend/CrmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.crm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
